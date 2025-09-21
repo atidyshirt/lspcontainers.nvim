58d80bd7a9a2c01edae75f8a34475d7adf2b7349
jordanp <jordanpyott@gmail.com>
2025-09-21 23:46:57 +1200

    Update to use atidyshirt registry with available LSPs
    - Updated registry from lspcontainers to atidyshirt
    - Removed LSPs not available in our registry
    - Updated LSP names to match our new naming convention:
      - tsserver -> ts_ls
      - sumneko_lua -> lua_ls
    - Kept only LSPs we have built: bashls, clangd, gopls, jsonls, lua_ls, pylsp, rust_analyzer, ts_ls, yamlls

--- lua/lspcontainers/init.lua
+++ lua/lspcontainers/init.lua
@@ -3,10 +3,8 @@ Config = {
 }
 
 local supported_languages = {
-    bashls = { image = "docker.io/lspcontainers/bash-language-server" },
-    clangd = { image = "docker.io/lspcontainers/clangd-language-server" },
-    denols = { image = "docker.io/lspcontainers/denols" },
-    dockerls = { image = "docker.io/lspcontainers/docker-language-server" },
+    bashls = { image = "docker.io/atidyshirt/bashls" },
+    clangd = { image = "docker.io/atidyshirt/clangd" },
     gopls = {
         cmd_builder = function(runtime, workdir, image, network)
             local volume = workdir .. ":" .. workdir .. ":z"
@@ -47,27 +45,14 @@ local supported_languages = {
                 image
             }
         end,
-        image = "docker.io/lspcontainers/gopls",
+        image = "docker.io/atidyshirt/gopls",
     },
-    graphql = { image = "docker.io/lspcontainers/graphql-language-service-cli" },
-    html = { image = "docker.io/lspcontainers/html-language-server" },
-    intelephense = { image = "docker.io/lspcontainers/intelephense" },
-    jsonls = { image = "docker.io/lspcontainers/json-language-server" },
-    omnisharp = { image = "docker.io/lspcontainers/omnisharp" },
-    powershell_es = { image = "docker.io/lspcontainers/powershell-language-server" },
-    prismals = { image = "docker.io/lspcontainers/prisma-language-server" },
-    pylsp = { image = "docker.io/lspcontainers/python-lsp-server" },
-    pyright = { image = "docker.io/lspcontainers/pyright-langserver" },
-    rust_analyzer = { image = "docker.io/lspcontainers/rust-analyzer" },
-    solargraph = { image = "docker.io/lspcontainers/solargraph" },
-    lemminx = { image = "docker.io/lspcontainers/lemminx" },
-    lua_ls = { image = "docker.io/lspcontainers/lua-language-server" },
-    svelte = { image = "docker.io/lspcontainers/svelte-language-server" },
-    tailwindcss = { image = "docker.io/lspcontainers/tailwindcss-language-server" },
-    terraformls = { image = "docker.io/lspcontainers/terraform-ls" },
-    tsserver = { image = "docker.io/lspcontainers/typescript-language-server" },
-    vuels = { image = "docker.io/lspcontainers/vue-language-server" },
-    yamlls = { image = "docker.io/lspcontainers/yaml-language-server" },
+    jsonls = { image = "docker.io/atidyshirt/jsonls" },
+    lua_ls = { image = "docker.io/atidyshirt/lua_ls" },
+    pylsp = { image = "docker.io/atidyshirt/pylsp" },
+    rust_analyzer = { image = "docker.io/atidyshirt/rust_analyzer" },
+    ts_ls = { image = "docker.io/atidyshirt/ts_ls" },
+    yamlls = { image = "docker.io/atidyshirt/yamlls" },
 }
 
 -- convert dos path to unix path
